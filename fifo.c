9c0ed9b4553999d74985ccaaf86d49ec4eb3fd9c
rdhananjaya <roshandhananjaya@gmail.com>
2013-05-24 20:43:50 +0530

    refacted the fifo.c test to a sepapate file fifo_test.c

--- fifo.c
+++ fifo.c
@@ -2,14 +2,8 @@
 #include <time.h>
 
 
-
-#define number_of_threads 6 
-#define number_of_threads_d 5
-#define number_of_iters 1000
-
-
 #define DEBUG
-//#undef DEBUG /*  uncomment when you are done! */
+#undef DEBUG /*  uncomment when you are done! */
   
 #ifdef DEBUG
     #define print printf
@@ -154,79 +148,6 @@ void * deque(queue * que)
     pthread_mutex_unlock(&mutex);
     print("Exiting deque\n");
     return data;
-}
-
-struct _stru {
-    int number;
-    int thread_no;
-    queue * q;
-};
-
-void * func(void * arg)
-{
-    struct _stru * args = (struct _stru *) arg;
-    int number = args->number;
-    int th_number = args->thread_no;
-    queue * q = args->q;
-
-    int i;
-    srand(time(NULL));
-    for(i = 0; i < number; i++) {
-        char * message = malloc(16);
-        snprintf(message, 15, "rand: %d", rand());
-        enque(q, (void *)message);
-    }
-    
-}
-
-void * func_d(void * args)
-{
-    print("Func_D running....\n");
-
-    queue * q = (queue *) args;
-
-    void * data;
-    while((data = deque(q)) != NULL) {
-        char * string = (char *)data;
-        print("DeQued : %s, @%p\n", string, data);
-        free(data);
-    }
-    print("Func_D exiting....\n");
-}
-
-int main() 
-{
-    queue * q = queue_factory();
-
-    pthread_t threads[number_of_threads];
-    pthread_t thread_d[number_of_threads_d];
-    
-    int i;
-    struct _stru arg[number_of_threads];
-    for(i = 0; i < number_of_threads; i++) {
-        arg[i].number = number_of_iters;
-        arg[i].thread_no = i;
-        arg[i].q = q;
-        pthread_create(threads+i, NULL, func, (void *)&arg[i]); 
-    }
-
-    for(i = 0; i < number_of_threads_d; i++) {
-        pthread_create(thread_d+i, NULL, func_d, (void *)q);
-    }
-
-    for(i = 0; i < number_of_threads; i++) {
-        pthread_join(*(threads+i), NULL);
-    }
-    
-    for(i = 0; i < number_of_threads_d; i++) {
-        pthread_join(*(thread_d+i), NULL);   
-    }
-
-    print("Going to run queue_destroy(...) \n");
-    queue_destroy(q);
-    print("que freeed... :) \n");
-
-    return 0;
 }
 
 
---
+++ fifo_test.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include "queue_ts.h"
+
+#define number_of_threads	6 
+#define number_of_threads_d 5
+#define number_of_iters		1000
+ 
+#define DEBUG
+//#undef DEBUG /*   uncomment when you are done! */
+ 
+#ifdef DEBUG
+	#define print printf
+#else
+    #define print(...)
+#endif
+
+
+
+struct _stru {
+    int number;
+    int thread_no;
+    queue * q;
+};
+ 
+void * func(void * arg)
+{
+    struct _stru * args = (struct _stru *) arg;
+    int number = args->number;
+    int th_number = args->thread_no;
+    queue * q = args->q;
+
+    int i;
+    srand(time(NULL));
+    for(i = 0; i < number; i++) {
+        char * message = malloc(16);
+        snprintf(message, 15, "rand: %d", rand());
+        enque(q, (void *)message);
+    }
+}
+
+
+void * func_d(void * args)
+{
+    print("Func_D running....\n");
+
+    queue * q = (queue *) args;
+
+    void * data;
+    while((data = deque(q)) != NULL) {
+        char * string = (char *)data;
+        print("DeQued : %s, @%p\n", string, data);
+        free(data);
+    }
+    print("Func_D exiting....\n");
+}
+
+int main() 
+{
+    queue * q = queue_factory();
+
+    pthread_t threads[number_of_threads];
+    pthread_t thread_d[number_of_threads_d];
+    
+    int i;
+    struct _stru arg[number_of_threads];
+    for(i = 0; i < number_of_threads; i++) {
+        arg[i].number = number_of_iters;
+        arg[i].thread_no = i;
+        arg[i].q = q;
+        pthread_create(threads+i, NULL, func, (void *)&arg[i]); 
+    }
+
+    for(i = 0; i < number_of_threads_d; i++) {
+        pthread_create(thread_d+i, NULL, func_d, (void *)q);
+    }
+
+    for(i = 0; i < number_of_threads; i++) {
+        pthread_join(*(threads+i), NULL);
+    }
+    
+    for(i = 0; i < number_of_threads_d; i++) {
+        pthread_join(*(thread_d+i), NULL);   
+    }
+
+    print("Going to run queue_destroy(...) \n");
+    queue_destroy(q);
+    print("que freeed... :) \n");
+
+    return 0;
+}
